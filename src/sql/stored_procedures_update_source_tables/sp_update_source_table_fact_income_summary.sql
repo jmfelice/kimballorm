295427f0b154ffbcf8c635cfd94f61832b2b83f6
Jared M. Felice <jmfel@fisherautoparts.com>
2024-08-04 21:34:31 -0400

    modified the cash flow table and the income summary table.  Last thing is the balance sheet.  After that I can start added tables to the raw integration part and then build out the reporting system.

--- src/sql/stored_procedures_update_source_tables/sp_update_source_table_fact_income_summary.sql
+++ src/sql/stored_procedures_update_source_tables/sp_update_source_table_fact_income_summary.sql
@@ -1,30 +1,49 @@
-CREATE OR REPLACE PROCEDURE finance_etl.sp_populate_source_table_fact_income_summary()
+CREATE OR REPLACE PROCEDURE finance_etl.sp_update_source_table_fact_income_summary()
+LANGUAGE plpgsql
 AS $$
 BEGIN
 
-INSERT INTO finance_etl.fact_income_summary_source
-(
+truncate table finance_etl.fact_income_summary_source;
+
+INSERT INTO finance_etl.fact_income_summary_source (
+    foreign_key_hash,
+    measures_hash,
     branch_key,
     gl_account_id_key,
     corporation_key,
     posting_date_key,
     debit_amount,
     credit_amount,
-    amount
+    amount,
+    action
 )
 
+with base as
+(
 select
-b.branch_key,
-re.gl_account_id_key,
-co.corporation_key,
+FNV_HASH(
+    CAST(coalesce(a.gl_account_id_key    , -1) AS VARCHAR) ||
+    CAST(coalesce(b.branch_key           , -1) AS VARCHAR) ||
+    CAST(coalesce(cp.corporation_key     , -1) AS VARCHAR) ||
+    CAST(coalesce(cast(date_part(year, f.glperiod) || 1231 as INT), -1) AS VARCHAR)
+) AS foreign_key_hash,
+FNV_HASH(
+    CAST(sum(f.rbdr) AS VARCHAR) ||
+    CAST(sum(f.rbcr) AS VARCHAR)
+    ) AS measures_hash,
+coalesce(re.gl_account_id_key, -1) as gl_account_id_key,
+coalesce(b.branch_key        , -1) as branch_key,
+coalesce(co.corporation_key  , -1) as corporation_key,
 cast(date_part(year, f.glperiod) || 1231 as INT) as posting_date_key,
-sum(f.rbdr) as debit_amount,
-sum(f.rbcr) as credit_amount,
-sum(f.rbcr - f.rbdr) as amount
+sum(coalesce(f.rbdr         , 0)) as debit_amount,
+sum(coalesce(f.rbcr         , 0)) as credit_amount,
+sum(coalesce(f.rbcr - f.rbdr, 0)) as amount
 from finance_staging.iseries_usrjmflib_arfadtv110 f
 left join finance_dw.dim_account a on a.gl_account_id = f.rbglcd
 left join finance_dw.dim_corporation co on co.corporation = f.rbcorp
-left join finance_dw.dim_branch b on b.branch = co.elimination_branch and cast(date_part(year, f.glperiod) || '-12-31' as DATE) between b.SCD2_start_date and b.scd2_end_date
+left join finance_dw.dim_branch b on
+    b.branch = co.elimination_branch and
+    cast(date_part(year, f.glperiod) || '-12-31' as DATE) between b.SCD2_start_date and b.scd2_end_date
 left join finance_dw.dim_account re on re.gl_account_id = '31'
 where
     f.rbcorp = 1
@@ -35,8 +54,62 @@ group by
     re.gl_account_id_key,
     co.corporation_key,
     cast(date_part(year, f.glperiod) || 1231 as INT)
+)
 
+SELECT
+    source.foreign_key_hash,
+    source.measures_hash,
+    source.gl_account_id_key,
+    source.branch_key,
+    source.corporation_key,
+    source.posting_date_key,
+    source.debit_amount,
+    source.credit_amount,
+    source.amount,
+    'INSERT' as action
+FROM base AS source
+LEFT OUTER JOIN finance_dw.fact_income_summary AS target ON
+    source.foreign_key_hash = target.foreign_key_hash
+WHERE target.foreign_key_hash is null
+
+union all
+
+SELECT
+    target.foreign_key_hash,
+    target.measures_hash,
+    target.gl_account_id_key,
+    target.branch_key,
+    target.corporation_key,
+    target.posting_date_key,
+    target.debit_amount,
+    target.credit_amount,
+    target.amount,
+    'DELETE' as action
+FROM base AS source
+LEFT OUTER JOIN finance_dw.fact_income_summary AS target ON
+    source.foreign_key_hash = target.foreign_key_hash
+WHERE source.foreign_key_hash is null
+
+union all
+
+SELECT
+    source.foreign_key_hash,
+    source.measures_hash,
+    source.gl_account_id_key,
+    source.branch_key,
+    source.corporation_key,
+    source.posting_date_key,
+    source.debit_amount,
+    source.credit_amount,
+    source.amount,
+    'UPDATE' as action
+FROM base AS source
+LEFT OUTER JOIN finance_dw.fact_income_summary AS target ON
+    source.foreign_key_hash = target.foreign_key_hash
+WHERE
+    source.foreign_key_hash is not null and
+    target.foreign_key_hash is not null and
+    source.measures_hash != target.measures_hash
 ;
-
 END;
-$$ LANGUAGE plpgsql;
+$$
--- src/sql/stored_procedures_update_target_tables/sp_update_target_table_fact_cash_flow.sql
+++ src/sql/stored_procedures_update_target_tables/sp_update_target_table_fact_cash_flow.sql
@@ -1,66 +1,36 @@
-CREATE OR REPLACE PROCEDURE finance_etl.sp_update_target_table_fact_acquisition_cash_flow()
+CREATE OR REPLACE PROCEDURE finance_etl.sp_update_target_table_fact_cash_flow()
 LANGUAGE plpgsql
 AS $$
 BEGIN
 
-with base as
-(
-    select
-    cast('general_ledger' as VARCHAR(30)) as type,
-    coalesce(f.gl_account_id_key               , -1) as gl_account_id_key,
-    coalesce(f.branch_key                      , -1) as branch_key,
-    coalesce(f.corporation_key                 , -1) as corporation_key,
-    coalesce(f.category_key                    , -1) as category_key,
-    coalesce(cf.indirect_cash_flow_category_key, -1) as indirect_cash_flow_category_key,
-    coalesce(f.posting_date_key                , -1) as posting_date_key
-    from finance_dw.fact_general_ledger f
-    left join finance_dw.bridge_map_cash_flow cf on cf.gl_account_id_key = f.gl_account_id_key
-    where cf.indirect_cash_flow_category_key is not null
-
-    union all
-
-    select
-    cast('acquisition' as VARCHAR(30)) as type,
-    -1                                              as gl_account_id_key,
-    -1                                              as branch_key,
-    coalesce(f.corporation_key                , -1) as corporation_key,
-    -1                                              as category_key ,
-    coalesce(i.indirect_cash_flow_category_key, -1) as indirect_cash_flow_category_key,
-    coalesce(f.posting_date_key               , -1) as posting_date_key
-    from finance_dw.fact_acquisition_cash_flow f
-    left join finance_dw.dim_corporation                  c on c.corporation_key = f.corporation_key
-    left join finance_dw.dim_calendar                    cl on cl.date_key = f.posting_date_key
-    left join finance_dw.dim_indirect_cash_flow_category  i on i.indirect_cash_flow_category_key = f.indirect_cash_flow_category_key
-    where i.indirect_cash_flow_category_key is not null
-)
-
-, delete_cte as
-(
-    SELECT
-    target.fact_cash_flow_key
-    FROM finance_dw.fact_cash_flow AS target
-    LEFT OUTER JOIN base AS source ON
-        source.gl_account_id_key               = target.gl_account_id_key               and
-        source.branch_key                      = target.branch_key                      and
-        source.corporation_key                 = target.corporation_key                 and
-        source.category_key                    = target.category_key                    and
-        source.indirect_cash_flow_category_key = target.indirect_cash_flow_category_key and
-        source.posting_date_key                = target.posting_date_key
-    WHERE
-        source.gl_account_id_key               is null or
-        source.branch_key                      is null or
-        source.corporation_key                 is null or
-        source.category_key                    is null or
-        source.indirect_cash_flow_category_key is null or
-        source.posting_date_key                is null
-)
+CALL finance_etl.sp_update_source_table_fact_cash_flow();
 
 DELETE FROM finance_dw.fact_cash_flow
-USING delete_cte
-WHERE finance_dw.fact_acquisition_cash_flow.fact_cash_flow_key = delete_cte.fact_cash_flow_key
+USING finance_etl.fact_cash_flow_source source
+WHERE action = 'DELETE' and source.foreign_key_hash = finance_dw.fact_cash_flow.foreign_key_hash
+;
+
+UPDATE finance_dw.fact_cash_flow
+SET
+    measures_hash                   = source.measures_hash,
+    gl_account_id_key               = source.gl_account_id_key,
+    branch_key                      = source.branch_key,
+    corporation_key                 = source.corporation_key,
+    category_key                    = source.category_key,
+    indirect_cash_flow_category_key = source.indirect_cash_flow_category_key,
+    posting_date_key                = posting_date_key.measures_hash,
+    general_ledger                  = source.general_ledger,
+    acquisition                     = source.acquisition,
+    cash_flow                       = source.cash_flow
+FROM finance_etl.fact_cash_flow_source source
+WHERE
+    source.action = 'UPDATE' and
+    source.foreign_key_hash = finance_dw.fact_cash_flow.foreign_key_hash
 ;
 
 INSERT INTO finance_dw.fact_cash_flow (
+    foreign_key_hash,
+    measures_hash,
     gl_account_id_key,
     branch_key,
     corporation_key,
@@ -72,95 +42,21 @@ INSERT INTO finance_dw.fact_cash_flow (
     cash_flow
 )
 
-with base as
-(
-    select
-    cast('general_ledger' as VARCHAR(30)) as type,
-    coalesce(f.gl_account_id_key               , -1) as gl_account_id_key,
-    coalesce(f.branch_key                      , -1) as branch_key,
-    coalesce(f.corporation_key                 , -1) as corporation_key,
-    coalesce(f.category_key                    , -1) as category_key,
-    coalesce(cf.indirect_cash_flow_category_key, -1) as indirect_cash_flow_category_key,
-    coalesce(f.posting_date_key                , -1) as posting_date_key
-    from finance_dw.fact_general_ledger f
-    left join finance_dw.bridge_map_cash_flow cf on cf.gl_account_id_key = f.gl_account_id_key
-    where cf.indirect_cash_flow_category_key is not null
+select
+foreign_key_hash,
+measures_hash,
+gl_account_id_key,
+branch_key,
+corporation_key,
+category_key ,
+indirect_cash_flow_category_key,
+posting_date_key,
+general_ledger,
+acquisition,
+cash_flow
+from finance_etl.fact_cash_flow_source
+where action = 'INSERT'
 
-    union all
-
-    select
-    cast('acquisition' as VARCHAR(30)) as type,
-    -1                                              as gl_account_id_key,
-    -1                                              as branch_key,
-    coalesce(f.corporation_key                , -1) as corporation_key,
-    -1                                              as category_key ,
-    coalesce(i.indirect_cash_flow_category_key, -1) as indirect_cash_flow_category_key,
-    coalesce(f.posting_date_key               , -1) as posting_date_key
-    from finance_dw.fact_acquisition_cash_flow f
-    left join finance_dw.dim_corporation                  c on c.corporation_key = f.corporation_key
-    left join finance_dw.dim_calendar                    cl on cl.date_key = f.posting_date_key
-    left join finance_dw.dim_indirect_cash_flow_category  i on i.indirect_cash_flow_category_key = f.indirect_cash_flow_category_key
-    where i.indirect_cash_flow_category_key is not null
-)
-
-, combined as
-(
-    select
-    type,
-    gl_account_id_key,
-    branch_key,
-    corporation_key,
-    category_key ,
-    indirect_cash_flow_category_key,
-    posting_date_key,
-    sum(amount) * -1 as amount
-    from base f
-
-    group by
-    type,
-    gl_account_id_key,
-    branch_key,
-    corporation_key,
-    category_key ,
-    indirect_cash_flow_category_key,
-    posting_date_key
-)
-
-, cash_flow as
-(
-    select
-    gl_account_id_key,
-    branch_key,
-    corporation_key,
-    category_key ,
-    indirect_cash_flow_category_key,
-    posting_date_key,
-    coalesce(general_ledger, 0) as general_ledger,
-    coalesce(acquisition, 0) as acquisition,
-    coalesce(general_ledger, 0) + coalesce(acquisition, 0) as cash_flow
-    from combined
-    PIVOT(SUM(amount) for type in ('general_ledger', 'acquisition'))
-)
-
-SELECT
-    source.gl_account_id_key,
-    source.branch_key,
-    source.corporation_key,
-    source.category_key ,
-    source.indirect_cash_flow_category_key,
-    source.posting_date_key,
-    source.general_ledger,
-    source.acquisition,
-    source.cash_flow
-FROM cash_flow AS source
-LEFT OUTER JOIN finance_dw.fact_cash_flow AS target ON
-    source.gl_account_id_key               = target.gl_account_id_key
-    AND source.branch_key                      = target.branch_key
-    AND source.corporation_key                 = target.corporation_key
-    AND source.category_key                    = target.category_key
-    AND source.indirect_cash_flow_category_key = target.indirect_cash_flow_category_key
-    AND source.posting_date_key                = target.posting_date_key
-WHERE target.fact_cash_flow_key IS NULL
 ;
 END;
 $$
